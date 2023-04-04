89864aa4083f47004330191c709eb45b664e21b3
bswji <104555111+bswji@users.noreply.github.com>
2023-04-04 19:14:14 +1000

    Update

--- AT2.py
+++ AT2.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
+#Load data
 data = pd.read_csv("/Users/brandonji/Documents/Uni/Sem 1 2023/Fundamentals of data analytics/FLY32130_DataSet_24579183.csv")
 
+#Create dataframe and remove id and unnamed:0 columns
 data.sample(1)
 data = pd.DataFrame(data)
 data = data.drop(columns = ["Unnamed: 0", "id"])
-data.sample(1)
 
-data.info()
+#Find unique values in customer type 
+data['Customer Type'].unique()
+
