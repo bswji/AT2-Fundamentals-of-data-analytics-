b17c018676c05aca4117a3362042c063977c7ff4
bswji <104555111+bswji@users.noreply.github.com>
2023-04-04 18:04:33 +1000

    Update AT2.py

--- AT2.py
+++ AT2.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
+data = pd.read_csv("/Users/brandonji/Documents/Uni/Sem 1 2023/Fundamentals of data analytics/FLY32130_DataSet_24579183.csv")
 
+data.sample(1)
+
+data = data.drop(columns = ["Unnamed: 0", "id"])
+data.sample(1)
 
